8ae78077430004ad7f1dd5110891665b06115c4d
Devon Stewart <dstewart@twilio.com>
2020-04-10 16:42:03 -0700

    Printing stubs for interface methods

--- rules/src/main/scala/fix/GuardrailInterface2TF.scala
+++ rules/src/main/scala/fix/GuardrailInterface2TF.scala
@@ -7,6 +7,7 @@ class GuardrailInterface2TF extends SemanticRule("GuardrailInterface2TF") {
   override def fix(implicit doc: SemanticDocument): Patch = {
     (doc.tree.collect { case target@q"class ${Type.Name(name)}[..$interfaceTargs](implicit $inj: InjectK[..$_]) { ..$body }" =>
       doc.tree.collect { case companion@q"object ${Term.Name(`name`)} { ${defnDef@Defn.Def(_, _, _, _, Some(Type.Apply(clsType, List(l@Type.Name("L"), f))), Term.New(classInit))} }" =>
+        println(s"Adapting $name syntax injector to TF")
 
         val newInitTpe = classInit.tpe match {
           case Type.Apply(cls, List(l, f)) => Type.Apply(cls, List(l, t"Free[$f, ?]"))
@@ -20,7 +21,10 @@ class GuardrailInterface2TF extends SemanticRule("GuardrailInterface2TF") {
         ).syntax)
 
         val newInterfaceBody = body.map {
-          case Defn.Def(mods, name, tparms, paramss, Some(t"Free[$f, $a]"), defn) => Decl.Def(mods, name, tparms, paramss, t"$f[$a]")
+          case defn@Defn.Def(mods, name, tparms, paramss, Some(t"Free[$f, $a]"), _) =>
+            val ret = Decl.Def(mods, name, tparms, paramss, t"$f[$a]")
+            println(s"  ${defn.copy(body=q"???").syntax}")
+            ret
           case other => other
         }
         val monadDecl = q"def MonadF: Monad[F]"
